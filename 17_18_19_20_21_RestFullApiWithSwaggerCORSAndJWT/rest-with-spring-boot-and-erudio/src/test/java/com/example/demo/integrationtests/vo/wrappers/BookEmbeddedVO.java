2e9c44bb8bf63f761c48f0b7cd47679d549058b8
Ademir <ademir_lopes@hotmail.com>
2025-01-11 07:02:23 -0300

    RestFullApiWithSwaggerCORSAndJWT

---
+++ 17_18_19_20_21_RestFullApiWithSwaggerCORSAndJWT/rest-with-spring-boot-and-erudio/src/test/java/com/example/demo/integrationtests/vo/wrappers/BookEmbeddedVO.java
@@ -0,0 +1,26 @@
+package com.example.demo.integrationtests.vo.wrappers;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import com.example.demo.integrationtests.vo.BookVO;
+
+public class BookEmbeddedVO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @JsonProperty("bookVOList")
+    private List<BookVO> books;
+
+    public BookEmbeddedVO() {}
+
+    public List<BookVO> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<BookVO> books) {
+        this.books = books;
+    }
+}
